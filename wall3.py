179e9a440550de485e0afe008046e5b69e0e1277
JJ Posti <jjposti@techtimejourney.net>
2025-03-03 02:08:31 +0200

    Patching the code to work again.

--- wall3.py
+++ wall3.py
@@ -1,17 +1,4 @@
-
-#!/usr/bin/env python3
-
-#Wallselector3 Copyright (c) 2019 JJ Posti <techtimejourney.net> 
-#Wallselector3  comes with ABSOLUTELY NO WARRANTY; 
-#for details see: http://www.gnu.org/copyleft/gpl.html. 
-#This is free software, and you are welcome to redistribute it under 
-#GPL Version 2, June 1991")
-
-#This is a simple wallpaper switching program. Coded with Python3 and Qt5. Requires feh.  
-
-
-
-import os, sys, sys, subprocess, getpass
+import os, sys, subprocess, getpass
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
@@ -21,33 +8,37 @@ class Main(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(Main, self).__init__(*args, **kwargs)
         self.setStyleSheet("*{color:#ffffff; background-color:#2f2e2d; border: 2px solid #353535; border-radius: 3px;font-size: 12px;}"
-"*:selected{background-color:#125c8c;}") 
+                           "*:selected{background-color:#125c8c;}")
         self.openFileNameDialog()
            
     def openFileNameDialog(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self,"Choose wallpaper", "/usr/share/wallpapers","All Files (*);;Python Files (*.py)", options=options)
+        fileName, _ = QFileDialog.getOpenFileName(self, "Choose wallpaper", "/usr/share/postx-wallpapers", "All Files (*);;Python Files (*.py)", options=options)
         if fileName:
             subprocess.Popen(['feh', '--bg-scale', fileName])
-            name=getpass.getuser()
-            uhome="/home/"
-            wall="/.wall/wall.sh"        
-            combine=uhome + name + wall
-            folder=uhome+name 
-            fehcommand="feh --bg-scale "
-            subprocess.Popen(['rm', '-r' , combine])
-            subprocess.Popen(['touch', combine])
-            os.chdir(folder)
-            f = open(combine, 'a')
-            f.write(fehcommand)
-            f.write(fileName)
-            f.write('\n')
-            f.write('\n')
-            f.close()
+            name = getpass.getuser()
+            uhome = "/home/"
+            wall = ".wall/wall.sh"        
+            combine = os.path.join(uhome, name, wall)
+            folder = os.path.join(uhome, name)
+            fehcommand = "feh --bg-scale "
+
+            # Create the folder if it doesn't exist
+            if not os.path.exists(os.path.dirname(combine)):
+                os.makedirs(os.path.dirname(combine))
+
+            if os.path.exists(combine):
+                os.remove(combine)
+
+            with open(combine, 'a') as f:
+                f.write(fehcommand)
+                f.write(fileName)
+                f.write('\n')
+
             sys.exit()
         else:
-            sys.exit()			                 
+            sys.exit()
     
 if __name__ == '__main__':
     app = QApplication(sys.argv)
